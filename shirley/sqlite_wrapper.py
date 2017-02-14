3950aa053bb1a1ee1217f552ea59ae4c2ba8ec95
Governor <elimushwari@gmail.com>
2017-02-14 17:10:59 +0300

    Set up the to_do class

--- shirley/sqlite_wrapper.py
+++ shirley/sqlite_wrapper.py
@@ -1,2 +1,28 @@
-class SqliteWrapper(object):
+import os
+import sys
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///todo.db')
+engine.echo = False  
+
+metadata = MetaData(engine)
+
+Base = declarative_base()
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+class TodoList(Base):
+	
+	__tablename__ = "todo_list"
+
+	id = Column(Integer, primary_key=True)
+
+	lists = Column(String(250), nullable=False)
+
 	
