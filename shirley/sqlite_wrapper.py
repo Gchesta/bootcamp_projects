48ceef33bdd031c92442d4844a19f1c6457cf5f6
Governor <elimushwari@gmail.com>
2017-02-15 04:04:00 +0300

    Separate the instantiated classes from the functions

--- shirley/sqlite_wrapper.py
+++ shirley/sqlite_wrapper.py
@@ -2,8 +2,10 @@ import os
 import sys
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+#from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
+#from sqlalchemy.ext.automap import automap_base
+from sqlite_tables import *
 
 
 engine = create_engine('sqlite:///todo.db')
@@ -17,35 +19,47 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-class TodoList(Base):
-	
-	__tablename__ = "todo_list"
+"""def sql_add_list(to_do_list):
+	new_list = TodoList(lists = to_do_list)
+	session.add(new_list)
+	session.commit()
 
-	id = Column(Integer, primary_key=True)
+def sql_add_item(to_do_list, item):
+	f = session.query(TodoList).filter_by(lists = to_do_list).one()
+	f_id = f.list_id
+	#new_list = TodoList(lists = to_do_list)
+	new_item = Items(items = item, list_id = f_id)
+	session.add(new_item)
+	session.commit()
 
-	lists = Column(String(250), nullable=False)
+def sql_view_lists():
+	my_lists = session.query(TodoList).all()
+	for my_list in my_lists:
+		print(str(my_list.list_id) + ". " + my_list.lists)
 
-class Items(Base):
-	
-	__tablename__ = "item_list"
 
-	id = Column(Integer, primary_key=True)
 
-	
-Base.metadata.create_all(engine)
+def sql_view_items(to_do_list):
+	f = session.query(TodoList).filter_by(lists = to_do_list).one()
+	f_id = f.list_id
+	my_items = session.query(Items).filter_by(list_id = f_id).all()
+	counter = 1
+	print(to_do_list)
+	for my_item in my_items:
+		print(str(counter) + ". " + my_item.items)
+		counter += 1"""
 
-class Action(object):
-	def __init__(self, to_do_list):
-		pass
+"""sql_add_item("Jobs to Apply To", "Teaching - TSC")
+sql_add_item("Books to Read", "The Souls of Black Folks")
+sql_add_item("Jobs to Apply To", "KQ Pilot")
+sql_add_item("Books to Read", "The Government Inspector")
+sql_add_item("Books to Read", "Midshipman Easy")
+sql_add_item("Jobs to Apply To", "Andela Fellowship")
+sql_add_item("Books to Read", "Decolonising the Mind")
+sql_add_item("Books to Read", "You Don't Know JS")
+sql_add_item("Books to Read", "Education for Self-Reliance")
+sql_add_item("Books to Read", "Utenzi wa Mwana Kupona")
+sql_add_item("Books to Read", "Bible")
 
-	def sql_add_list(to_do_list):
-		new_list = TodoList(lists = to_do_list)
-		session.add(new_list)
-		session.commit()
+sql_view_items("Books to Read")"""
 
-	def sql_add_item(to_do_list, item):
-		new_item = Items(items = item)
-		session.add(new_item)
-		session.commit()
-
-		pass
