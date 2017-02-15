2961e0fe6be21349ab381704544e2afbe58ed95a
Governor <elimushwari@gmail.com>
2017-02-15 08:53:00 +0300

    Test whether sql_add_item can add an item

--- shirley/tests_sqlite_wrapper.py
+++ shirley/tests_sqlite_wrapper.py
@@ -20,9 +20,15 @@ class TestSqliteWrapper(unittest.TestCase):
 	def test_sql_add_list(self):
 		q = "Tests to Run"
 		sql_add_list(q)
-		self.assertEqual(True, session.query(TodoList).filter_by(lists = q).count())
+		self.assertEqual(True, session.query(TodoList).filter_by(lists = q).count() >= 1)
 
-		
+	def test_sql_add_item_by_list_name(self):
+		q = "Tests to Run"
+		r = "Check test_sql_add_list"
+		sql_add_item(q, r)
+		self.assertEqual(True, session.query(Items).filter_by(items = r).count() >= 1)
+
+
 if __name__ == "__main__":
     unittest.main()
 
