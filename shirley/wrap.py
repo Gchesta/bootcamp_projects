dedd17b763af64d77293ebf24cd895da73dc4c49
Governor <elimushwari@gmail.com>
2017-02-16 16:50:52 +0300

    enable error handling

--- shirley/wrap.py
+++ shirley/wrap.py
@@ -34,14 +34,18 @@ class SqliteWrapper(object):
 
 	def sql_add_item(to_do_list, item):
 		"""A function to add items to a to-do list"""
-		if type(to_do_list) != int:
-			f = session.query(TodoList).filter_by(lists = to_do_list).one()
-			f_id = f.list_id
-		else:
-			f_id = to_do_list
-		new_item = Items(items = item, list_id = f_id)
-		session.add(new_item)
-		session.commit()
+		if to_do_list != None and item != None:
+			try:
+				if type(to_do_list) != int:
+					f = session.query(TodoList).filter_by(lists = to_do_list).one()
+					f_id = f.list_id
+				else:
+					f_id = to_do_list
+				new_item = Items(items = item, list_id = f_id)
+				session.add(new_item)
+				session.commit()
+			except Exception:
+				print("YOU HAVE INPUTED WRONG ARGUMENTS")
 
 	def sql_view_lists():
 		"""A function to view all the to-do lists"""
@@ -54,19 +58,20 @@ class SqliteWrapper(object):
 		
 	def sql_view_items(to_do_list):
 		"""A function to view the data in a to-do list"""
-		if type(to_do_list) != int:
-			f = session.query(TodoList).filter_by(lists = to_do_list).one()
-			f_id = f.list_id
-		else:
-			f_id = to_do_list
-		my_items = session.query(Items).filter_by(list_id = f_id).all()
-		counter = 1
-		data = [["No.", to_do_list]]
-		for my_item in my_items:
-			data.append([counter, my_item.items])
-			counter += 1
-		table = AsciiTable(data)
-		print(table.table)
+		if to_do_list != None and item != None:
+			try:
+				if type(to_do_list) != int:
+					f = session.query(TodoList).filter_by(lists = to_do_list).one()
+					f_id = f.list_id
+				else:
+					f_id = to_do_list
+				my_items = session.query(Items).filter_by(list_id = f_id).all()
+				counter = 1
+				data = [["No.", to_do_list]]
+				for my_item in my_items:
+					data.append([counter, my_item.items])
+					counter += 1
+				table = AsciiTable(data)
+				print(table.table)
 
-
-
+		
