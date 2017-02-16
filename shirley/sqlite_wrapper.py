1ad33efed05a46724ccdc75213b3d0bd4fb1a61f
Governor <elimushwari@gmail.com>
2017-02-16 10:34:26 +0300

    Ipliment the list_id check on sql_view_items

--- shirley/sqlite_wrapper.py
+++ shirley/sqlite_wrapper.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 #from sqlalchemy.ext.automap import automap_base
 from sqlite_tables import *
 from terminaltables import AsciiTable
-
+#from shirley import Shirley
 
 #declarations
 engine = create_engine('sqlite:///todo.db')
@@ -20,58 +20,49 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+class SqliteWrapper(object):
 
-def sql_add_list(to_do_list):
-	"""A function to add lists to the todo_list table"""
-	new_list = TodoList(lists = to_do_list)
-	session.add(new_list)
-	session.commit()
+	def __init__ (self):
+		pass
 
-def sql_add_item(to_do_list, item):
-	"""A function to add items to a to-do list"""
-	f = session.query(TodoList).filter_by(lists = to_do_list).one()
-	f_id = f.list_id
-	new_item = Items(items = item, list_id = f_id)
-	session.add(new_item)
-	session.commit()
+	def sql_add_list(to_do_list):
+		"""A function to add lists to the todo_list table"""
+		new_list = TodoList(lists = to_do_list)
+		session.add(new_list)
+		session.commit()
+		
 
-def sql_view_lists():
-	"""A function to view all the to-do lists"""
-	my_lists = session.query(TodoList).all()
-	for my_list in my_lists:
-		print(str(my_list.list_id) + ". " + my_list.lists)
+	def sql_add_item(to_do_list, item):
+		"""A function to add items to a to-do list"""
+		f = session.query(TodoList).filter_by(lists = to_do_list).one()
+		f_id = f.list_id
+		new_item = Items(items = item, list_id = f_id)
+		session.add(new_item)
+		session.commit()
 
-def sql_view_items(to_do_list):
-	"""A function to view the data in a to-do list"""
-	f = session.query(TodoList).filter_by(lists = to_do_list).one()
-	f_id = f.list_id
-	my_items = session.query(Items).filter_by(list_id = f_id).all()
-	counter = 1
-	data = [["No", to_do_list]]
-	for my_item in my_items:
-		data.append([counter, my_item.items])
-		counter += 1
-	table = AsciiTable(data)
-	print(table.table)
+	def sql_view_lists():
+		"""A function to view all the to-do lists"""
+		my_lists = session.query(TodoList).all()
+		data = [["No.", "Lists"]]
+		for my_list in my_lists:
+			data.append([my_list.list_id, my_list.lists])
+		table = AsciiTable(data)
+		print(table.table)
+		
+	def sql_view_items(to_do_list):
+		"""A function to view the data in a to-do list"""
+		if type(to_do_list) != int:
+			f = session.query(TodoList).filter_by(lists = to_do_list).one()
+			f_id = f.list_id
+		else:
+			f_id = to_do_list
+		my_items = session.query(Items).filter_by(list_id = f_id).all()
+		counter = 1
+		data = [["No.", to_do_list]]
+		for my_item in my_items:
+			data.append([counter, my_item.items])
+			counter += 1
+		table = AsciiTable(data)
+		print(table.table)
 
-sql_view_items("Books to Read")
 
-"""sql_add_list("Jobs to Apply To")
-sql_add_list("Books to Read")
-
-sql_add_item("Jobs to Apply To", "Teaching - TSC")
-sql_add_item("Books to Read", "The Souls of Black Folks")
-sql_add_item("Jobs to Apply To", "KQ Pilot")
-sql_add_item("Books to Read", "The Government Inspector")
-sql_add_item("Books to Read", "Midshipman Easy")
-sql_add_item("Jobs to Apply To", "Andela Fellowship")
-sql_add_item("Books to Read", "Decolonising the Mind")
-sql_add_item("Books to Read", "You Don't Know JS")
-sql_add_item("Books to Read", "Education for Self-Reliance")
-sql_add_item("Books to Read", "Utenzi wa Mwana Kupona")
-sql_add_item("Books to Read", "Bible")
-
-sql_view_items("Books to Read")
-sql_view_items("Jobs to Apply To")
-
-sql_view_lists()"""
