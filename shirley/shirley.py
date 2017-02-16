ada001fb74770c10bd609fd45805a0b61f913b7e
Governor <elimushwari@gmail.com>
2017-02-16 10:28:59 +0300

    Add control .isdigit() control to todo open

--- shirley/shirley.py
+++ shirley/shirley.py
@@ -43,30 +43,33 @@ class Shirley(object):
 			print("USE COMMAND 'todo open' BEFORE USING COMMAND 'item add'") #check for the above error
 			Shirley.shirley_start()
 
-		elif command == "todo open":
+		elif command == "todo open": #check the todo open command
 			if arguments:
-				entries_item = input(":")
+				entries_item = input(":") 
 				command_item =  " ".join(entries_item.split(" ")[0:2])
 				arguments_item = " ".join(entries_item.split(" ")[2:])
 
 				if command_item == "item add":
 					if arguments_item:
+						if arguments.isdigit():
+							arguments = int(arguments) #will send a list id
+						else:
+							arguments = arguments #will send a list
 						SqliteWrapper.sql_add_item(arguments, arguments_item)
 						Shirley.shirley_start()
 
 					else:
-						print("INVALID ARGUMENTSSSSSSS. TRY AGAIN")
+						print("INVALID ARGUMENTS. TRY AGAIN")
 						Shirley.shirley_start()
 				else:
 					print(command_item)
 					print("INVALID COMMAND. TRY AGAIN")
 					Shirley.shirley_start()
 
-			
 			else:
 				print("INVALID ARGUMENTS. TRY AGAIN")
 				Shirley.shirley_start()
-		"""else:
+		else:
 			print("INVALID COMMANDS. TRY AGAIN")
 			Shirley.shirley_start()
 
